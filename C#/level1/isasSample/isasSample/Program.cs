77dcc738e32db5193d04cadc40cdeb3852853124
lijianing <v_lijianing@bilibili.com>
2026-05-11 19:41:50 +0800

    Sample

---
+++ C#/level1/isasSample/isasSample/Program.cs
@@ -0,0 +1,55 @@
+﻿namespace isasSample
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //object o = new Teacher();
+            //Teacher t = o as Teacher;
+            //if (t != null)
+            //{
+            //    t.Teach();
+            //}
+
+            object o = new Teacher();
+            Console.WriteLine(o.GetType().Name);
+            if(o is Teacher)
+            {
+                Teacher t = (Teacher)o;
+                t.Teach();
+            }
+        }
+    }
+
+    class Animal
+    {
+        public void Eat()
+        {
+            Console.WriteLine("I like eatting!");
+        }
+    }
+
+    class Human:Animal
+    {
+        public void Think()
+        {
+            Console.WriteLine("Who I am?");
+        }
+    }
+
+    class Teacher:Human
+    {
+        public void Teach()
+        {
+            Console.WriteLine("I teach Programming!");
+        }
+    }
+
+    class Car
+    {
+        public void Speed()
+        {
+            Console.WriteLine("Go! Go! Go!");
+        }
+    }
+}
