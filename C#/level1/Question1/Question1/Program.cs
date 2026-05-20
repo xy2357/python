9f81688a9b1f8f344794b01153a80c97b19d2bba
xy1257 <1909411819@qq.com>
2026-05-20 09:46:11 +0800

    level1

---
+++ C#/level1/Question1/Question1/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Question1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int x = 10;
+            int y = x;
+            y = 20;
+
+            Console.WriteLine(x);
+            Console.WriteLine(y);
+        }
+    }
+}
---
+++ C#/level1/Question2/Question2/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace Question2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string a = "Hello";
+            string b = a;
+            b = "World";
+
+            Console.WriteLine(a);
+            Console.WriteLine(b);
+        }
+    }
+}
